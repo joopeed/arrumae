b0b4921055367e5c759663f34952ffba70bae04c
leticiawanderley <leticia.wanderley@ccc.ufcg.edu.br>
2014-11-23 18:12:54 -0200

    Primitive geolocalization working

--- app/src/br/edu/ufcg/les142/InitialActivity.java
+++ app/src/br/edu/ufcg/les142/InitialActivity.java
@@ -2,8 +2,11 @@ package br.edu.ufcg.les142;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import com.google.android.gms.maps.SupportMapFragment;
 
 public class InitialActivity extends FragmentActivity {
+
+    private SupportMapFragment mapa;
     /*
      * Initialize the Activity
      */
@@ -11,5 +14,9 @@ public class InitialActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        mapa = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        // Enable the current location "blue dot"
+        mapa.getMap().setMyLocationEnabled(true);
     }
 }
