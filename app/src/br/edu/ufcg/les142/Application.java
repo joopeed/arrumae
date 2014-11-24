fcf93a80f8c3724231f8ca7a72e36ff6260dbac4
Lucas Cavalcante <lucasmcavalcante@gmail.com>
2014-11-24 06:44:55 -0300

    T05, localizacao faltando adicionar zoom

--- app/src/br/edu/ufcg/les142/Application.java
+++ app/src/br/edu/ufcg/les142/Application.java
@@ -1,14 +1,29 @@
 package br.edu.ufcg.les142;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import com.parse.Parse;
 
-/**
- * Created by Leticia on 15/11/2014.
- */
+
 public class Application extends android.app.Application {
     public static final String APPTAG = "les-142";
 
+    // Key for saving the search distance preference
+    private static final String KEY_SEARCH_DISTANCE = "searchDistance";
+
+    private static final float DEFAULT_SEARCH_DISTANCE = 250.0f;
+
+    private static SharedPreferences preferences;
+
+
     public void onCreate() {
         Parse.initialize(this, "tvOukSi0AaVPFCsEhBT1NcHbOK1q9CtANbPSPLhM", "LcFGhcfgRL631bzFXFfkxebgJTAurZYQWLADfB8n");
+        preferences = getSharedPreferences("com.parse.anywall", Context.MODE_PRIVATE);
     }
+
+
+//    public static float getSearchDistance() {
+//        preferences.toString();
+//        return preferences.getFloat(KEY_SEARCH_DISTANCE, DEFAULT_SEARCH_DISTANCE);
+//    }
 }
--- app/src/br/edu/ufcg/les142/InitialActivity.java
+++ app/src/br/edu/ufcg/les142/InitialActivity.java
@@ -1,22 +1,337 @@
 package br.edu.ufcg.les142;
 
+import android.annotation.SuppressLint;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.IntentSender;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.location.LocationClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.parse.ParseGeoPoint;
 
-public class InitialActivity extends FragmentActivity {
+public class InitialActivity extends FragmentActivity implements LocationListener ,
+        GooglePlayServicesClient.ConnectionCallbacks,
+        GooglePlayServicesClient.OnConnectionFailedListener{
 
+
+    /*
+    * Define a request code to send to Google Play services This code is returned in
+    * Activity.onActivityResult
+    */
+    private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
+
+    // The update interval
+    private static final int UPDATE_INTERVAL_IN_SECONDS = 5;
+
+    /*
+ * Constants for handling location results
+ */
+    // Conversion from feet to meters
+    private static final float METERS_PER_FEET = 0.3048f;
+
+    // Milliseconds per second
+    private static final int MILLISECONDS_PER_SECOND = 1000;
+
+    // Update interval in milliseconds
+    private static final long UPDATE_INTERVAL_IN_MILLISECONDS = MILLISECONDS_PER_SECOND
+            * UPDATE_INTERVAL_IN_SECONDS;
+
+    // A fast interval ceiling
+    private static final int FAST_CEILING_IN_SECONDS = 1;
+
+    // A fast ceiling of update intervals, used when the app is visible
+    private static final long FAST_INTERVAL_CEILING_IN_MILLISECONDS = MILLISECONDS_PER_SECOND
+            * FAST_CEILING_IN_SECONDS;
+
+    // Initial offset for calculating the map bounds
+    private static final double OFFSET_CALCULATION_INIT_DIFF = 1.0;
+
+    // Accuracy for calculating the map bounds
+    private static final float OFFSET_CALCULATION_ACCURACY = 0.01f;
+
+    private boolean hasSetUpInitialLocation;
+
+    private Location currentLocation;
+    private Location lastLocation;
+
+
+    // private SupportMapFragment mapa;
+    private LocationRequest locationRequest;
+
+    // Stores the current instantiation of the location client in this object
+    private LocationClient locationClient;
+
+    // Fields for the map radius in feet
+    private float radius;
+
+    // Map fragment
     private SupportMapFragment mapa;
+
+
     /*
      * Initialize the Activity
      */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        radius = Application.getSearchDistance();
         setContentView(R.layout.main);
 
+
+
+        // Create a new global location parameters object
+        locationRequest = LocationRequest.create();
+        locationRequest.setInterval(UPDATE_INTERVAL_IN_MILLISECONDS);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setFastestInterval(FAST_INTERVAL_CEILING_IN_MILLISECONDS);
+
+        // Create a new location client, using the enclosing class to handle callbacks.
+        locationClient = new LocationClient(this, this, this);
+
+        // Set up the map fragment
         mapa = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         // Enable the current location "blue dot"
         mapa.getMap().setMyLocationEnabled(true);
+}
+    @Override
+    public void onLocationChanged(Location location) {
+        currentLocation = location;
+        if (lastLocation != null
+                && geoPointFromLocation(location)
+                .distanceInKilometersTo(geoPointFromLocation(lastLocation)) < 0.01) {
+            return;
+        }
+        lastLocation = location;
+//        LatLng myLatLng = new LatLng(location.getLatitude(), location.getLongitude());
+//        if (!hasSetUpInitialLocation) {
+//            // Zoom to the current location.
+//            updateZoom(myLatLng);
+//            hasSetUpInitialLocation = true;
+//        }
+    }
+
+    /*
+    * Zooms the map to show the area of interest based on the search radius
+    */
+    private void updateZoom(LatLng myLatLng) {
+        // Get the bounds to zoom to
+        LatLngBounds bounds = calculateBoundsWithCenter(myLatLng);
+        // Zoom to the given bounds
+        mapa.getMap().animateCamera(CameraUpdateFactory.newLatLngBounds(bounds, 5));
+    }
+
+    /*
+    * Helper method to calculate the bounds for map zooming
+    */
+    LatLngBounds calculateBoundsWithCenter(LatLng myLatLng) {
+        // Create a bounds
+        LatLngBounds.Builder builder = LatLngBounds.builder();
+
+        // Calculate east/west points that should to be included
+        // in the bounds
+        double lngDifference = calculateLatLngOffset(myLatLng, false);
+        LatLng east = new LatLng(myLatLng.latitude, myLatLng.longitude + lngDifference);
+        builder.include(east);
+        LatLng west = new LatLng(myLatLng.latitude, myLatLng.longitude - lngDifference);
+        builder.include(west);
+
+        // Calculate north/south points that should to be included
+        // in the bounds
+        double latDifference = calculateLatLngOffset(myLatLng, true);
+        LatLng north = new LatLng(myLatLng.latitude + latDifference, myLatLng.longitude);
+        builder.include(north);
+        LatLng south = new LatLng(myLatLng.latitude - latDifference, myLatLng.longitude);
+        builder.include(south);
+
+        return builder.build();
+    }
+
+    /*
+ * Helper method to calculate the offset for the bounds used in map zooming
+ */
+    private double calculateLatLngOffset(LatLng myLatLng, boolean bLatOffset) {
+        // The return offset, initialized to the default difference
+        double latLngOffset = OFFSET_CALCULATION_INIT_DIFF;
+        // Set up the desired offset distance in meters
+        float desiredOffsetInMeters = radius * METERS_PER_FEET;
+        // Variables for the distance calculation
+        float[] distance = new float[1];
+        boolean foundMax = false;
+        double foundMinDiff = 0;
+        // Loop through and get the offset
+        do {
+            // Calculate the distance between the point of interest
+            // and the current offset in the latitude or longitude direction
+            if (bLatOffset) {
+                Location.distanceBetween(myLatLng.latitude, myLatLng.longitude, myLatLng.latitude
+                        + latLngOffset, myLatLng.longitude, distance);
+            } else {
+                Location.distanceBetween(myLatLng.latitude, myLatLng.longitude, myLatLng.latitude,
+                        myLatLng.longitude + latLngOffset, distance);
+            }
+            // Compare the current difference with the desired one
+            float distanceDiff = distance[0] - desiredOffsetInMeters;
+            if (distanceDiff < 0) {
+                // Need to catch up to the desired distance
+                if (!foundMax) {
+                    foundMinDiff = latLngOffset;
+                    // Increase the calculated offset
+                    latLngOffset *= 2;
+                } else {
+                    double tmp = latLngOffset;
+                    // Increase the calculated offset, at a slower pace
+                    latLngOffset += (latLngOffset - foundMinDiff) / 2;
+                    foundMinDiff = tmp;
+                }
+            } else {
+                // Overshot the desired distance
+                // Decrease the calculated offset
+                latLngOffset -= (latLngOffset - foundMinDiff) / 2;
+                foundMax = true;
+            }
+        } while (Math.abs(distance[0] - desiredOffsetInMeters) > OFFSET_CALCULATION_ACCURACY);
+        return latLngOffset;
+    }
+
+    private void startPeriodicUpdates() {
+        locationClient.requestLocationUpdates(locationRequest, this);
+    }
+
+    private void stopPeriodicUpdates() {
+        locationClient.removeLocationUpdates(this);
+    }
+
+    private Location getLocation() {
+        if (servicesConnected()) {
+            return locationClient.getLastLocation();
+        } else {
+            return null;
+        }
+    }
+    /*
+ * Helper method to get the Parse GEO point representation of a location
+ */
+    private ParseGeoPoint geoPointFromLocation(Location loc) {
+        return new ParseGeoPoint(loc.getLatitude(), loc.getLongitude());
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        currentLocation = getLocation();
+        startPeriodicUpdates();
+    }
+
+    @Override
+    public void onDisconnected() {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        if (connectionResult.hasResolution()) {
+            try {
+                connectionResult.startResolutionForResult(this, CONNECTION_FAILURE_RESOLUTION_REQUEST);
+            } catch (IntentSender.SendIntentException e) {
+            }
+        } else {
+            showErrorDialog(connectionResult.getErrorCode());
+        }
+    }
+
+    private boolean servicesConnected() {
+        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+
+        if (ConnectionResult.SUCCESS == resultCode) {
+            return true;
+        } else {
+            Dialog dialog = GooglePlayServicesUtil.getErrorDialog(resultCode, this, 0);
+            if (dialog != null) {
+                ErrorDialogFragment errorFragment = new ErrorDialogFragment();
+                errorFragment.setDialog(dialog);
+               // errorFragment.show(getSupportFragmentManager(), Application.APPTAG);
+            }
+            return false;
+        }
+    }
+
+    /*
+ * Show a dialog returned by Google Play services for the connection error code
+ */
+    private void showErrorDialog(int errorCode) {
+        // Get the error dialog from Google Play services
+        Dialog errorDialog =
+                GooglePlayServicesUtil.getErrorDialog(errorCode, this,
+                        CONNECTION_FAILURE_RESOLUTION_REQUEST);
+
+        // If Google Play services can provide an error dialog
+        if (errorDialog != null) {
+
+            // Create a new DialogFragment in which to show the error dialog
+            ErrorDialogFragment errorFragment = new ErrorDialogFragment();
+
+            // Set the dialog in the DialogFragment
+            errorFragment.setDialog(errorDialog);
+
+            // Show the error dialog in the DialogFragment
+            //errorFragment.show(getSupportFragmentManager(), Application.APPTAG);
+        }
+    }
+
+    @Override
+    public void onStop() {
+        if (locationClient.isConnected()) {
+            stopPeriodicUpdates();
+        }
+        locationClient.disconnect();
+
+        super.onStop();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        locationClient.connect();
+    }
+
+    @SuppressLint("NewApi")
+    public static class ErrorDialogFragment extends DialogFragment {
+        // Global field to contain the error dialog
+        private Dialog mDialog;
+
+        /**
+         * Default constructor. Sets the dialog field to null
+         */
+        public ErrorDialogFragment() {
+            super();
+            mDialog = null;
+        }
+
+        /*
+         * Set the dialog to display
+         *
+         * @param dialog An error dialog
+         */
+        public void setDialog(Dialog dialog) {
+            mDialog = dialog;
+        }
+
+        /*
+         * This method must return a Dialog to the DialogFragment.
+         */
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            return mDialog;
+        }
     }
 }
