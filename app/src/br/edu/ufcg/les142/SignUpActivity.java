989f3d7906a0f17e4c5c19121977379e1a302d42
leticiawanderley <leticia.wanderley@ccc.ufcg.edu.br>
2014-12-10 19:27:18 -0300

    Fixing CPF validation

--- app/src/br/edu/ufcg/les142/SignUpActivity.java
+++ app/src/br/edu/ufcg/les142/SignUpActivity.java
@@ -148,6 +148,13 @@ public class SignUpActivity extends Activity {
      *      true se o CPF é válido, false caso contrário
      */
     private boolean validaCPF(String cpf) {
+        if (cpf.equals("00000000000") || cpf.equals("11111111111") ||
+                cpf.equals("22222222222") || cpf.equals("33333333333") ||
+                cpf.equals("44444444444") || cpf.equals("55555555555") ||
+                cpf.equals("66666666666") || cpf.equals("77777777777") ||
+                cpf.equals("88888888888") || cpf.equals("99999999999")) {
+            return false;
+        }
         int multiplicador = 10;
         int primeiroDigito = 0;
         int segundoDigito = 0;
