2a234f50c17dca3f5d3495973cdd30a20b8793d5
rodrigofariasm <rodrigohfarias@gmail.com>
2015-03-10 23:19:09 -0300

    some bugs fixed

--- app/src/br/edu/ufcg/les142/LoginActivity.java
+++ app/src/br/edu/ufcg/les142/LoginActivity.java
@@ -70,7 +70,7 @@ public class LoginActivity extends Activity {
 
         // Validate the log in data
         boolean validationError = false;
-        StringBuilder validationErrorMessage = new StringBuilder(getString(R.string.error_intro));
+        final StringBuilder validationErrorMessage = new StringBuilder(getString(R.string.error_intro));
         if (username.length() == 0) {
             validationError = true;
             validationErrorMessage.append(getString(R.string.error_blank_username));
@@ -84,12 +84,6 @@ public class LoginActivity extends Activity {
         }
         validationErrorMessage.append(getString(R.string.error_end));
 
-        // If there is a validation error, display the error
-        if (validationError) {
-            Toast.makeText(LoginActivity.this, validationErrorMessage.toString(), Toast.LENGTH_LONG)
-                    .show();
-            return;
-        }
 
         // Set up a progress dialog
         final ProgressDialog dialog = new ProgressDialog(LoginActivity.this);
@@ -102,7 +96,10 @@ public class LoginActivity extends Activity {
                 dialog.dismiss();
                 if (e != null) {
                     // Show the error message
-                    Toast.makeText(LoginActivity.this, e.getMessage(), Toast.LENGTH_LONG).show();
+                    if (e.getMessage().equals("invalid login credentials")){
+                        Toast.makeText(LoginActivity.this, R.string.invalid_login_credentials, Toast.LENGTH_LONG).show();
+                    }
+
                 } else {
                     // Start an intent for the dispatch activity
                     Intent intent = new Intent(LoginActivity.this, DispatchActivity.class);
